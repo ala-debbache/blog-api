970a2ede97892aca00d81b15925914a259fe1c6c
ala-debbache <65853803+ala-debbache@users.noreply.github.com>
2021-10-02 10:36:37 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const mongodb = require("mongodb");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const router = require("./routes");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors({
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE"
+}));
+dotenv.config();
+app.use("/posts",router);
+
+mongoose.connect(process.env.MONGO_URI,()=>console.log("we're connected to the database"));
+
+
+
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname+"/public/index.html");
+});
+
+app.listen(8080||process.env.PORT,()=>console.log(`server runing on http://localhost:${8080||process.env.PORT}`));
---
+++ routes.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const Post = require("./model.js");
+
+const router = express.Router();
+
+// get all posts from DB
+router.get("/",async (req,res)=>{
+    try {
+        const posts = await Post.find({});
+        console.log(posts);
+        res.json(posts);
+    } catch (err) {
+        res.json(err);
+    }
+});
+
+// get a single spesific post from DB
+router.get("/:id",async (req,res)=>{
+    const id = req.params.id;
+    try {
+        const post = await Post.findOne({_id: id});
+        console.log(post);
+        res.json(post);
+    } catch (err) {
+        res.json(err);
+    }
+});
+
+// create new post
+router.post("/",async (req,res)=>{
+    try {
+        console.log(req.body);
+        const data = Post(req.body);
+        await data.save();
+        console.log(data);
+        res.json(data);
+    } catch (err) {
+        res.json(err);
+    }
+});
+
+// update a spesific post
+router.patch("/:id",async (req,res)=>{
+    const id = req.params.id;
+    try {
+        const result = await Post.updateOne({_id: id}, req.body);
+        console.log(result);
+        res.json(result);
+    } catch (err) {
+        res.json(err);
+    }
+});
+
+// delete a spesific post
+router.delete("/:id",async (req,res)=>{
+    const id = req.params.id;
+    try {
+        const result = await Post.deleteOne({_id: id});
+        console.log(result);
+        res.json(result);
+    } catch (err) {
+        res.json(err);
+    }
+});
+
+module.exports = router;
